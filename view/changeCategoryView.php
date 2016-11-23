f4ee2f46480965951431c085c7077bf82b512aee
PierreThb <thube.pierre@gmail.com>
2016-11-23 18:35:48 +0100

    FIX [changeCategoryView] url changed to fix bug

--- view/changeCategoryView.php
+++ view/changeCategoryView.php
@@ -9,35 +9,8 @@
 ?>
 <html>
     <div>
-        <datalist id="category">
-            <option value="NASA"></option>
-            <option value="Fish & Wildlife"></option>
-            <option value="Ireland"></option>
-            <option value="User Pics"></option>
-            <option value="Work"></option>
-            <option value="Airshow"></option>
-            <option value="Plants"></option>
-            <option value="Wild Animal"></option>
-            <option value="Family"></option>
-            <option value="Los Angeles"></option>
-            <option value="Home"></option>
-            <option value="SCA"></option>
-            <option value="Cooking"></option>
-            <option value="Balboa Park"></option>
-            <option value="Misc"></option>
-            <option value="Festivals"></option>
-            <option value="Recreation"></option>
-            <option value="Ocean"></option>
-            <option value="San Diego"></option>
-            <option value="SD Zoo"></option>
-            <option value="Jon"></option>
-            <option value="Tourist"></option>
-            <option value="Beltaine"></option>
-            <option value="Attractions"></option>
-        </datalist>
-
-        <h1>Chamgement de catégorie de l'image</h1>
-        <form method="post" action="index.php?controller=photo&action=validateChangeCategory">
+        <h1>Changement de catégorie de l'image</h1>
+        <form method="post" action="index.php?controller=photo&action=validateChangeCategory&imageId=<?= $data->imageId?>">
             <p>id: <?= $data->imageId ?></p>
             <p>Catégorie actuelle: <?= $data->actualCategory ?></p>
             <select name="category">
@@ -49,24 +22,3 @@
         </form>
     </div>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
